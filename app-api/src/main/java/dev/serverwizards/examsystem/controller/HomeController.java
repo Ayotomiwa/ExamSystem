0ded7ec767c99e368bebe1c1a63708ff5d1fd4b0
Ayotomiwa <aomope2@gmail.com>
2023-05-04 05:37:04 +0100

    changes to homecontroller

--- app-api/src/main/java/dev/serverwizards/examsystem/controller/HomeController.java
+++ app-api/src/main/java/dev/serverwizards/examsystem/controller/HomeController.java
@@ -1,17 +1,21 @@
 package dev.serverwizards.examsystem.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpResponse;
+
 
 
 @RestController
-@RequestMapping("/")
 public class HomeController{
+
+  @GetMapping("/")
   public ResponseEntity<String> homeGreeting(){
-    return ResponseEntity.ok("Hello World");
+    for (int i =0; i< 20; i++){
+      System.out.println("Welcome to the Exam System API **WINK WINK**");
+    }
+    return ResponseEntity.ok("Welcome to the Exam System API **WINK WINK**");
   }
 
 }
